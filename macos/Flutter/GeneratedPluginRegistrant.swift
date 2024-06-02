4ecc68c9b008e7b9642122bcfc54c99601a5db8a
Ahmad Tariq <75003816+ahmadbintariq4u@users.noreply.github.com>
2024-06-02 17:08:51 +0800

    Fix Gradle Issues

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_timezone
-import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
